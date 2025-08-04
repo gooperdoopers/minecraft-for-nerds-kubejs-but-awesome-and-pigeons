2341216b49959707d19c74fa5c60669422529009
gooperdoopers <165457297+gooperdoopers@users.noreply.github.com>
2025-08-04 02:12:29 -0700

    Add files via upload

---
+++ kubejs/server_scripts/delete.js
@@ -0,0 +1,12 @@
+ServerEvents.recipes(event => {
+    event.remove({ output: 'minecraft:oak_boat' })
+    event.remove({ output: 'minecraft:spruce_boat' })
+    event.remove({ output: 'minecraft:birch_boat' })
+    event.remove({ output: 'minecraft:jungle_boat' })
+    event.remove({ output: 'minecraft:acacia_boat' })
+    event.remove({ output: 'minecraft:dark_oak_boat' })
+    event.remove({ output: 'minecraft:mangrove_boat' })
+    event.remove({ output: 'minecraft:cherry_boat' })
+
+
+    })
